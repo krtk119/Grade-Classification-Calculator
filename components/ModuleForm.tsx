755db6f7ab1a1d03c5db57fe8a55a90d7d22f0c9
krtk119 <119kartik@gmail.com>
2026-07-10 13:35:18 +0100

    Add handleAddModule function and add Module button, wire up

--- components/ModuleForm.tsx
+++ components/ModuleForm.tsx
@@ -1,13 +1,31 @@
 "use client";
+
+import { Module } from "@/types";      
 import { useState } from "react";
 
 export default function ModuleForm() {
   const [name, setName] = useState("");
   const [grade, setGrade] = useState("");
   const [credits, setCredits] = useState("");
-  
+  const [modules, setModules] = useState<Module[]>([]);
+  const handleAddModule = () => {
+    if (name && credits) {
+      const newModule: Module = {
+        id: crypto.randomUUID(),
+        name: name,
+        grade: Number(grade),
+        credits: Number(credits),
+        isResit: false,
+      };
+      setModules([...modules, newModule]);
+      setName("");
+      setGrade("");
+      setCredits("");
+    }
+  };
   return (
     <div>
+      
       <input
         type="text"
         value={name}
@@ -29,6 +47,13 @@ export default function ModuleForm() {
         placeholder="Credits"
         className="border rounded px-3 py-2"
       />
+      <button
+  onClick={handleAddModule}
+  className="bg-blue-600 text-white px-4 py-2 rounded"
+>
+  Add Module
+</button>
+
       
     </div>
   );
